0c5c747fb7fc5598ebc53692ce3ac362162f6944
ventus.laminae <ventus.laminae@gmail.com>
2024-05-22 22:01:53 +0900

    現行資産を反映

---
+++ public/style/style.js
@@ -0,0 +1,8 @@
+/* global bootstrap: false */
+(function () {
+    'use strict'
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
